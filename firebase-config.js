6c2a8176ef40163247758e6c766e14a2624d649c
Amine4332 <112389874+Amine4332@users.noreply.github.com>
2025-08-02 23:25:49 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 // IMPORTANT: This file is in .gitignore and should NOT be committed to version control.
 // Replace the placeholder with your NEW, restricted API key.
 const firebaseConfig = {
-    apiKey: "YOUR_NEW_RESTRICTED_API_KEY_HERE",
+    apiKey: "AIzaSyCC23SltL1DPhTcXdZivR5sAKOc4o2l4K0",
     authDomain: "manal-hbd.firebaseapp.com",
     databaseURL: "https://manal-hbd-default-rtdb.firebaseio.com",
     projectId: "manal-hbd",
